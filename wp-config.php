681e60787645c3cc57a1d87f03f49e195acef430
Gray <gray@searchengines.ru>
2016-07-15 01:49:12 +0300

    disable wp-cron

--- wp-config.php
+++ wp-config.php
@@ -32,6 +32,8 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+define('DISABLE_WP_CRON', true);
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
